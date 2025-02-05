1e7c1e17530391d49b76568941b39ee357a6c523
Aidan Killer <69479112+aikill@users.noreply.github.com>
2025-02-05 08:44:47 -0600

    Initial upload
    This is the first version of this program that is stable and tested

---
+++ Form1.Designer.vb
@@ -0,0 +1,99 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(form1))
+        Browse = New Button()
+        download = New Button()
+        downloadProgress = New ProgressBar()
+        DataGridView1 = New DataGridView()
+        TextBox1 = New TextBox()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Browse
+        ' 
+        Browse.Location = New Point(12, 12)
+        Browse.Name = "Browse"
+        Browse.Size = New Size(94, 29)
+        Browse.TabIndex = 0
+        Browse.Text = "Browse..."
+        Browse.UseVisualStyleBackColor = True
+        ' 
+        ' download
+        ' 
+        download.Location = New Point(542, 12)
+        download.Name = "download"
+        download.Size = New Size(94, 29)
+        download.TabIndex = 1
+        download.Text = "Download"
+        download.UseVisualStyleBackColor = True
+        ' 
+        ' downloadProgress
+        ' 
+        downloadProgress.Location = New Point(12, 49)
+        downloadProgress.Name = "downloadProgress"
+        downloadProgress.Size = New Size(624, 29)
+        downloadProgress.TabIndex = 2
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Location = New Point(12, 84)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.RowHeadersWidth = 51
+        DataGridView1.Size = New Size(624, 384)
+        DataGridView1.TabIndex = 4
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(112, 13)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(424, 27)
+        TextBox1.TabIndex = 5
+        ' 
+        ' form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(649, 480)
+        Controls.Add(TextBox1)
+        Controls.Add(DataGridView1)
+        Controls.Add(downloadProgress)
+        Controls.Add(download)
+        Controls.Add(Browse)
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        Name = "form1"
+        Text = "Bulk Image Downloader"
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Browse As Button
+    Friend WithEvents download As Button
+    Friend WithEvents downloadProgress As ProgressBar
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents TextBox1 As TextBox
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,163 @@
+﻿Imports System.IO
+Imports System.Net
+Imports System.Net.Http
+Imports Microsoft.VisualBasic.FileIO
+
+Public Class form1
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Browse.Click
+        Dim openFileDialog As New OpenFileDialog()
+        openFileDialog.Filter = "CSV Files (*.csv)|*.csv"
+        openFileDialog.Title = "Select a CSV File"
+
+        If openFileDialog.ShowDialog() = DialogResult.OK Then
+            ' Get the selected file path
+            Dim filePath As String = openFileDialog.FileName
+            TextBox1.Text = $"{filePath}"
+            ' Read the CSV file and store columns in lists
+            Dim columns As List(Of List(Of String)) = ReadCSVFile(filePath)
+
+            ' Display the data in the DataGridView
+            DisplayDataInGridView(columns)
+        Else
+            MessageBox.Show("No file selected.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+    End Sub
+
+    Private Sub mainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Function ReadCSVFile(filePath As String) As List(Of List(Of String))
+        ' Initialize a list of lists to store column data
+        Dim columns As New List(Of List(Of String))()
+
+        ' Use TextFieldParser to read the CSV file
+        Using parser As New TextFieldParser(filePath)
+            parser.TextFieldType = FieldType.Delimited
+            parser.SetDelimiters(",")
+
+            ' Read the header row and ignore it
+            If Not parser.EndOfData Then
+                parser.ReadFields()
+            End If
+
+            ' Read the rest of the file
+            While Not parser.EndOfData
+                Dim fields() As String = parser.ReadFields()
+
+                ' Ensure columns are initialized
+                If columns.Count = 0 Then
+                    For i As Integer = 0 To fields.Length - 1
+                        columns.Add(New List(Of String)())
+                    Next
+                End If
+
+                ' Add each field to its corresponding column
+                For i As Integer = 0 To fields.Length - 1
+                    columns(i).Add(fields(i))
+                Next
+            End While
+        End Using
+
+        Return columns
+    End Function
+
+    ' Function to display column data in the DataGridView
+    Private Sub DisplayDataInGridView(columns As List(Of List(Of String)))
+        ' Clear existing data in the DataGridView
+        DataGridView1.Columns.Clear()
+        DataGridView1.Rows.Clear()
+
+        ' Add columns to the DataGridView
+        For i As Integer = 0 To columns.Count - 1
+            DataGridView1.Columns.Add($"Column{i + 1}", $"Column {i + 1}")
+        Next
+
+        ' Add rows to the DataGridView
+        For rowIndex As Integer = 0 To columns(0).Count - 1
+            Dim rowValues(columns.Count - 1) As String
+            For colIndex As Integer = 0 To columns.Count - 1
+                rowValues(colIndex) = columns(colIndex)(rowIndex)
+            Next
+            DataGridView1.Rows.Add(rowValues)
+        Next
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+
+    End Sub
+
+    Private Async Sub download_Click(sender As Object, e As EventArgs) Handles download.Click
+        ' Create a folder to save the downloaded files
+        Dim dateTime As String = DateString + "_" + Format(Now, "hhmmss")
+        Dim saveFolder As String = Path.Combine(Application.StartupPath, "DownloadedFiles_" + dateTime)
+        'verify wheter a directory of the same name exists already or not 
+        If Not Directory.Exists(saveFolder) Then
+            Directory.CreateDirectory(saveFolder)
+            'if a directory with the same name exists then a randomly generated integer will be appended to the end of the file to prevent an exception
+        ElseIf Directory.Exists(saveFolder) Then
+            Dim randomInt As Integer = Int((100 * Rnd()) + 1)
+            Dim randomIntStr As String = randomInt.ToString
+            Directory.CreateDirectory(saveFolder + randomIntStr)
+        End If
+
+        'get a  count of rows in the datagrid
+        Dim rowCount As Integer = DataGridView1.Rows.Count
+        Dim progressIter As Integer = 100 / rowCount - 1
+
+        'reset the value of the progress bar to 0 
+        downloadProgress.Value = 0
+
+        'establish output file for errrors
+        Dim fs = CreateObject("Scripting.FileSystemObject")
+        Dim a = fs.CreateTextFile(Application.StartupPath + dateTime + "_failed downloads" + ".txt", True)
+
+
+        ' Create an HttpClient instance
+        Using client As New HttpClient()
+            ' Iterate through each row in the DataGridView
+            For Each row As DataGridViewRow In DataGridView1.Rows
+                ' Get the URL from the first column
+                Dim url As String = row.Cells(0).Value?.ToString()
+                If Not String.IsNullOrEmpty(url) Then
+                    Try
+                        ' Download the file
+                        Dim response As HttpResponseMessage = Await client.GetAsync(url)
+                        response.EnsureSuccessStatusCode()
+
+                        ' Get the file name from the second column if it exists
+                        Dim fileName As String = row.Cells(1).Value?.ToString()
+                        If String.IsNullOrEmpty(fileName) Then
+                            ' Use the default file name from the URL
+                            fileName = Path.GetFileName(url)
+                        Else
+                            ' Append the file extension from the URL
+                            Dim extension As String = Path.GetExtension(url)
+                            fileName = fileName & extension
+                        End If
+
+                        ' Save the file
+                        Dim filePath As String = Path.Combine(saveFolder, fileName)
+                        Dim fileBytes As Byte() = Await response.Content.ReadAsByteArrayAsync()
+                        File.WriteAllBytes(filePath, fileBytes)
+
+                        ' MessageBox.Show($"File downloaded and saved as: {filePath}", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                        downloadProgress.Value += progressIter
+                    Catch ex As Exception
+                        'inform the user that a download has failed and insert the url into a textfile with a similar naming convention to the download folder
+                        MessageBox.Show($"Failed to download file from {url}: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                        a.WriteLine("Failed to download file: " + url)
+                        downloadProgress.Value += progressIter
+                    End Try
+                End If
+            Next
+        End Using
+        a.close()
+        downloadProgress.Value = 100
+        MessageBox.Show("Download Process Complete!")
+    End Sub
+
+
+End Class
